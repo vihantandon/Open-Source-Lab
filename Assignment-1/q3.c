5669d528d2ee01e1851e68516d03ea3a78123753
vihantandon <156824356+vihantandon@users.noreply.github.com>
2025-07-26 11:05:57 +0530

    Create q3.c

---
+++ Assignment-1/q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
